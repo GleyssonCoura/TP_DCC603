075e9d0cb4167a6a650366ac9e86b4508d3de794
guilhermegbm <guilhermebmendonca60@gmail.com>
2021-06-20 16:31:16 -0300

    Camada Repository criada e configurada

---
+++ tp1-backend/src/main/java/com/engsoft/tp1backend/repository/UsuarioADMRepository.java
@@ -0,0 +1,9 @@
+package com.engsoft.tp1backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.engsoft.tp1backend.entity.UsuarioADM;
+
+public interface UsuarioADMRepository extends JpaRepository<UsuarioADM, Long> {
+
+}
