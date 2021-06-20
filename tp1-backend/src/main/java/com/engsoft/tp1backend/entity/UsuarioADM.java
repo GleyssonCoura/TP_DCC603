1ce6bec692ed63ceeeb08c499ac8448f75c0bd76
guilhermegbm <guilhermebmendonca60@gmail.com>
2021-06-20 16:25:22 -0300

    Mapeamento da Entidade Secao finalizado

--- tp1-backend/src/main/java/com/engsoft/tp1backend/entity/UsuarioADM.java
+++ tp1-backend/src/main/java/com/engsoft/tp1backend/entity/UsuarioADM.java
@@ -1,11 +1,18 @@
 package com.engsoft.tp1backend.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
@@ -16,7 +23,7 @@ public class UsuarioADM {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
+	@Column(name = "id", nullable = false)
 	private Long id;
 
 	@Column(name = "login", nullable = false)
@@ -27,4 +34,8 @@ public class UsuarioADM {
 
 	@Column(name = "nome", nullable = false)
 	private String nome;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "usuarioADMAutor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Secao> secoes;
 }
