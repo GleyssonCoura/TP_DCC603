ea82fb9e56f14aecb13a49305e007b0fed82ee97
guilhermegbm <guilhermebmendonca60@gmail.com>
2021-06-21 01:58:30 -0300

    Método de login para Usuário adm finalizado

--- tp1-backend/src/main/java/com/engsoft/tp1backend/controller/UsuarioADMController.java
+++ tp1-backend/src/main/java/com/engsoft/tp1backend/controller/UsuarioADMController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.engsoft.tp1backend.entity.UsuarioADM;
@@ -23,5 +25,10 @@ public class UsuarioADMController {
 	@PostMapping(path = "/inserirUsuarioADM")
 	public ResponseEntity<UsuarioADM> inserirUsuarioADM(@RequestBody(required = false) UsuarioADM usuarioADM) throws Exception {
 		return new ResponseEntity<UsuarioADM>(this.usuarioADMService.inserirUsuarioADM(usuarioADM), HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/fazerLogin")
+	public ResponseEntity<String> fazerLogin(@RequestParam(name = "login", required = false) String login, @RequestParam(name = "senha", required = false) String senha) throws Exception {
+		return new ResponseEntity<String>(this.usuarioADMService.fazerLogin(login, senha), HttpStatus.OK);
 	}
 }
--- tp1-backend/src/main/java/com/engsoft/tp1backend/service/UsuarioADMService.java
+++ tp1-backend/src/main/java/com/engsoft/tp1backend/service/UsuarioADMService.java
@@ -1,5 +1,7 @@
 package com.engsoft.tp1backend.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,5 +33,29 @@ public class UsuarioADMService {
 		}
 
 		return this.usuarioADMRepository.save(usuarioADM);
+	}
+
+	public String fazerLogin(String login, String senha) throws Exception {
+		if (login == null || login.isEmpty()) {
+			throw new EngSoftException("Login não pode ser vazio");
+		}
+
+		if (senha == null || senha.isEmpty()) {
+			throw new EngSoftException("Senha não pode ser vazia");
+		}
+
+		List<UsuarioADM> usuADMPorLogin = this.usuarioADMRepository.listarPorLogin(login);
+
+		if (usuADMPorLogin == null || usuADMPorLogin.isEmpty()) {
+			throw new EngSoftException("Nenhum usuário encontrado para o login " + login);
+		} else if (usuADMPorLogin.size() > 1) {
+			throw new EngSoftException("Mais de um usuário encontrado para o login " + login);
+		}
+
+		if (!senha.equals(usuADMPorLogin.get(0).getSenha())) {
+			throw new EngSoftException("Senha incorreta");
+		}
+
+		return "OK";
 	}
 }
