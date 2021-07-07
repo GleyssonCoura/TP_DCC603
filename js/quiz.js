37c983596c337e5d3be0f81ea8c47836d3184400
Rafaellfig <rafaellfigueiredo1@gmail.com>
2021-07-06 21:43:44 -0300

    adicionando a tela login

---
+++ js/quiz.js
@@ -0,0 +1,105 @@
+var myQuestions = [
+	{
+		question: "Qual a é a média dessa sequência de números? <br> (4,7,3,1,5,6,3,9,2,10)<br>",
+		answers: {
+			a: '6',
+			b: '5',
+			c: '3'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "Qual deles representa o valor central de um conjunto de dados?<br>",
+		answers: {
+			a: 'Média',
+			b: 'Moda',
+			c: 'Mediana'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "Qual a moda dessa sequência de números? <br> (2,3,5,3,2,6,7,2,9,12)<br>",
+		answers: {
+			a: '2',
+			b: '3',
+			c: '5',
+			d: '12'
+		},
+		correctAnswer: 'a'
+	}
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+		var output = [];
+		var answers;
+
+		for(var i=0; i<questions.length; i++){
+			
+			answers = [];
+
+			for(letter in questions[i].answers){
+
+				answers.push(
+					'<label>'
+						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+						+ questions[i].answers[letter]
+					+ '</label>'
+				);
+			}
+
+			output.push(
+				'<div class="question">' + questions[i].question + '</div>'
+				+ '<div class="answers">' + answers.join('') + '</div>'
+			);
+		}
+
+		quizContainer.innerHTML = output.join('');
+	}
+
+
+	function showResults(questions, quizContainer, resultsContainer){
+			
+		
+		var answerContainers = quizContainer.querySelectorAll('.answers');
+		
+		
+		var userAnswer = '';
+		var numCorrect = 0;
+		
+		
+		for(var i=0; i<questions.length; i++){
+
+			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+			
+			if(userAnswer===questions[i].correctAnswer){
+				
+				numCorrect++;
+				
+				
+				answerContainers[i].style.color = 'lightgreen';
+			}
+			
+			else{
+				
+				answerContainers[i].style.color = 'red';
+			}
+		}
+
+			
+			resultsContainer.innerHTML = numCorrect + ' de ' + questions.length;
+		}
+
+	showQuestions(questions, quizContainer);
+
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
+}
